9ec24e83c49caa057691ac389aa01763a8650264
shailesh gontewar <shaileshsgontewar@gmail.com>
2023-03-07 14:21:16 +0530

    fileadd

--- main.js
+++ main.js
@@ -6,18 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
     // console.log(response);
 
     for (let i = 0; i < response.length; i++) {
-      let descri = response[i].description;
       let title = response[i].title;
       product.innerHTML += ` <div class="product position-relative">
       <img src="${response[i].image}" alt="" class="product_image">
-      <span class="position-absolute top-2 right-3">  <i class="fa fa-heart"></i></span>
+      
       <div class="content">
       <h6 class="p_title">${title.length > 20 ? title.slice(0, 20) : title}</h6>
-      <small class="product_category">${response[i].category}</small>
-      <p class="product_description">${
-        descri.length > 20 ? descri.slice(0, 30) : descri
-      }....</p>
-      <div class="star text-center">
+     
+      <div class="star">
       <i class="fa-solid fa-star"></i>
       <i class="fa-solid fa-star"></i>
       <i class="fa-solid fa-star"></i>
---
+++ notes.md
@@ -0,0 +1,5 @@
+ <!-- <small class="product_category">${response[i].category}</small>
+      <p class="product_description">${
+        descri.length > 20 ? descri.slice(0, 30) : descri
+      }....</p> -->
+<!-- <span class="position-absolute top-0 end-0">  <i class="fa fa-heart"></i></span> -->
